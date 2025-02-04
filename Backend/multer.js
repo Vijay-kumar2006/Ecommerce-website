41d139c805d1b7e043836443b56b2eec98d9f930
Vijay-kumar2006 <vijaykumar.vk3105@gmail.com>
2025-02-04 13:13:58 +0530

    Nm2

--- Backend/multer.js
+++ Backend/multer.js
@@ -1,18 +1,30 @@
 const multer = require('multer');
 
 
-// Configure multer storage
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, './src/upload'); // Define your upload folder
+      cb(null, 'upload/'); 
     },
     filename: function(req, file, cb) {
       const uniqueSuffix =  Date.now() + '-' + Math.round.apply(Math.random() * 1e9);
-       // Define a unique filename
+      
        const filename = file.originalname.split(".")[0];
-       cb(null,filename + "-" + uniqueSuffix + ".png"); // Define
+       cb(null,filename + "-" + uniqueSuffix + ".png"); 
+    },
+  });
+
+
+  const productStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'productupload/'); 
+    },
+    filename: function(req, file, cb) {
+      const uniqueSuffix =  Date.now() + '-' + Math.round.apply(Math.random() * 1e9);
+      
+       const filename = file.originalname.split(".")[0];
+       cb(null,filename + "-" + uniqueSuffix + ".png"); 
     },
   });
   
-  // Initialize upload object
 exports.upload = multer({ storage: storage });
+exports.productupload = multer({ storage: productStorage });
---
+++ Backend/src/Controllers/products.js
@@ -0,0 +1,38 @@
+const {router} = require('express');
+const { productupload } = require('../../multer');
+const productrouter = router();
+
+productrouter.get("/", (req, res) => {
+    res.send("Product Router");
+});
+
+productrouter.post("/",productupload.array('files'),async(req, res) => {
+    const {name, description, category, tags, price, stock, email} = req.body;
+    const images = req.files.map((file) => file.path);
+    try{
+        const product = await productModel.findone({email:email});
+        if(!seller){
+            return res.status(400).json({message:"Seller not found"});
+        }
+        if(images.length === 0){
+            return res.status(400).json({message:"Please upload atleast one images"});
+        }
+        await productModel.create({
+            name:name,
+            description:description,
+            category:category,
+            tags:tags,
+            price:price,
+            stock:stock,
+            email:email,
+            images:images
+        });
+    }
+    catch(err){
+        console.log(err);
+    }
+    res.status(200).json({message:"Product added successfully"});
+
+});
+
+module.exports = productrouter;
