d50d5869ea32729c875bee5e7802d36a60c078e4
Vijay-kumar2006 <vijaykumar.vk3105@gmail.com>
2025-02-15 11:19:16 +0530

    check

--- FrontendNew/my/src/App.jsx
+++ FrontendNew/my/src/App.jsx
@@ -1,24 +1,31 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Router, Routes } from 'react-router-dom'
 import Login from './Components/Login'
 import { Signup } from './Components/Signup'
 import { Home } from './page/Home'
 import { ProductForm } from './Components/Productform'
 import { Productcardseller } from './Components/productcardforseller'
+import Navbar from './Components/Navbar'
 
 
 function App() {
  
  return (
     <>
-    <Routes>
+      <Navbar/>
+    <Router>
+      <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/Login" element={<Login/>}/>
       <Route path="/Signup" element={<Signup/>}/>
-   <Route path='/productform' element={<ProductForm/>}/>
-   <Route path='/my-product' element={<Productcardseller/>}/>
+     <Route path='/productform' element={<ProductForm/>}/>
+     <Route path='/my-product' element={<Productcardseller/>}/>
     </Routes>
+  </Router>
     </>
+
+   
+    
     
   )
 }
